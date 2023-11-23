983aeee487f681e637d12047ff3fbe073004775f
Full Stack Developer <webdeveloper2009@outlook.com>
2023-11-22 23:57:36 -0800

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import { appButtonCn, appCn, appTextCn } from './App.const';
+
+import './App.scss';
+
+export const App: React.FC = () => {
+    const [count, setCount] = React.useState<number>(0);
+
+    const onIncrement = React.useCallback(() => setCount(count + 1), [count]);
+
+    return (
+        <div className={appCn}>
+            <p className={appTextCn}>Hello, world!</p>
+            <button
+                type="button"
+                className={appButtonCn}
+                onClick={onIncrement}
+            >
+                {count}
+            </button>
+        </div>
+    );
+};
